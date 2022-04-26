2655f42452678074549a048f26ba9a5b4af9e269
iBel251 <ibbimran7@gmail.com>
2022-04-26 23:42:23 +0300

    Fix linter errors

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,19 +1,19 @@
 module.exports = {
-    ci: {
-      collect: {
-        // collect options here
-      },
-      assert: {
-        // assert options here
-      },
-      upload: {
-        // upload options here
-      },
-      server: {
-        // server options here
-      },
-      wizard: {
-        // wizard options here
-      },
+  ci: {
+    collect: {
+      // collect options here
     },
-  };
+    assert: {
+      // assert options here
+    },
+    upload: {
+      // upload options here
+    },
+    server: {
+      // server options here
+    },
+    wizard: {
+      // wizard options here
+    },
+  },
+};
