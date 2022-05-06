c380b0e21950637228869ea5943fe5d61d99eb74
iBel251 <ibbimran7@gmail.com>
2022-05-07 02:08:52 +0300

    Add unit tests for the remaining 3 functions

--- src/__mocks__/add-remove.js
+++ src/__mocks__/add-remove.js
@@ -21,8 +21,33 @@ function deleteTask(id) {
   return listContainer;
 }
 
+function editTask(id, name) {
+  const newName = name;
+  const taskName = listContainer[id].description;
+  if (taskName !== newName) {
+    listContainer[id].description = newName;
+    localStorage.setItem('container', JSON.stringify(listContainer));
+  }
+  return listContainer;
+}
+
+function updateStatus(id) {
+  listContainer[id].completed = !listContainer[id].completed;
+  localStorage.setItem('container', JSON.stringify(listContainer));
+  return listContainer;
+}
+
+function clearAllCompleted() {
+  listContainer = listContainer.filter((item) => item.completed === false);
+  localStorage.setItem('container', JSON.stringify(listContainer));
+  return listContainer;
+}
+
 module.exports = {};
 module.exports.addTask = addTask;
 module.exports.deleteTask = deleteTask;
+module.exports.editTask = editTask;
+module.exports.updateStatus = updateStatus;
+module.exports.clearAllCompleted = clearAllCompleted;
 module.exports.listContainer = listContainer;
 module.exports.Task = Task;
